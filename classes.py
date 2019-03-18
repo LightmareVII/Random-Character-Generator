2939461a2dd27da4646f8fed8d4dc6d3952fab6e
LightmareVII <48626681+LightmareVII@users.noreply.github.com>
2019-03-17 21:43:30 -0400

    Moved Main character class to main. Updated PlayerRace.Human

--- classes.py
+++ classes.py
@@ -1,50 +1,23 @@
 #Classes
-class PlayerCharacter(object):
-    def __init__(self,Name,Job,Race):
-        self.PlayerName = Name
-        self.PlayerJob = Job.Name
-        self.Proficiencies = []
-        #self.Background
-        #self.PlayerSubclass = Class.Subclass
-        #self.PlayerRace = Race.Name
-        self.PlayerLevel = 1
-        self.ProficiencyBonus = 2
-        self.ProficientStats = []
-        self.ClassNumber = 1
-        self.Stats = {"Str":roll_stats(),
-                      "Dex":roll_stats(),
-                      "Con":roll_stats(),
-                      "Int":roll_stats(),
-                      "Wis":roll_stats(),
-                      "Cha":roll_stats()}
-        self.height = 0 #Inches
-        self.weight = 0 #Pounds
-
-    class spells():
-        level_1 = []
-        level_2 = []
-        level_3 = []
-        level_4 = []
-        level_5 = []
-        level_6 = []
-        level_7 = []
-        level_8 = []
-        level_9 = []
-
-    def Add_Stats(self,stat,amount):
-        stat += amount #NEED TO FIGURE THIS OUT
-
-    def Add_Proficiency(self,Job):
-        for x in Job.proficiency:
-            self.Proficiencies += Job.proficiency[x]
-        #use for loop to grab list of proficiencies and
 
 #Player Race configuration options
-class PlayerRace():
+class PlayerRace(object):
 	class Human():
-		SkinColor = ["Light", "Medium", "Dark"]
+        Name = "Human"
+		SkinColor = ["White", "Tan", "Black","Ginger"]
 		Sex = ["Male", "Female"]
-        Age_Max = 100
+        Age = range(15,100)
+        Height = range(60,80)
+        weight = range(100,300)
+        Size = "Medium"
+        Speed = 30
+        Languages = ['Common','Extra']
+        Stats = {"Str":1,
+                 "Dex":1,
+                 "Con":1,
+                 "Int":1,
+                 "Wis":1,
+                 "Cha":1,}
 	class Goblin():
 		SkinColor = ["Green", "Yellow", "Brown"]
 		Sex = ["Male", "Female"]
@@ -58,7 +31,7 @@ class PlayerRace():
 		Sex = ["Male", "Female"]
         Age_Max = 50
 
-class PlayableJobs(object):
+class PlayerJobs(object):
     class Barbarian():
         Name = "Barbarian"
         HitDice = 12
--- main.py
+++ main.py
@@ -1,2 +1,42 @@
-import classes.py
+import classes.py #This isn't gonna work...how to do?
+import functions.py #This isn't gonna work...how to do?
 #########################################################################
+class PlayerCharacter(object):
+    def __init__(self,Name,Job,Race):
+        self.PlayerName = Name
+        self.PlayerJob = Job.Name
+        self.PlayerRace = Race.Name
+        self.Proficiencies = []
+        #self.Background
+        #self.PlayerSubclass = Class.Subclass
+        self.PlayerLevel = 1
+        self.ProficiencyBonus = 2
+        self.ProficientStats = []
+        self.JobNumber = 1 #Number of dnd classes in character (for multiclassing)
+        self.Stats = {"Str":roll_stats(),
+                      "Dex":roll_stats(),
+                      "Con":roll_stats(),
+                      "Int":roll_stats(),
+                      "Wis":roll_stats(),
+                      "Cha":roll_stats()}
+        self.Height = 0 #Inches
+        self.Weight = 0 #Pounds
+
+    class spells():
+        level_1 = []
+        level_2 = []
+        level_3 = []
+        level_4 = []
+        level_5 = []
+        level_6 = []
+        level_7 = []
+        level_8 = []
+        level_9 = []
+
+    def Add_Stats(self,stat,amount):
+        stat += amount #NEED TO FIGURE THIS OUT
+
+    def Add_Proficiency(self,Job):
+        for x in Job.proficiency:
+            self.Proficiencies += Job.proficiency[x]
+        #use for loop to grab list of proficiencies and
